eae5e38ef3d8db7fa55e45dbe8f5983e51ec2b0c
Leon-Long-Portfolio <153784947+Leon-Long-Portfolio@users.noreply.github.com>
2024-01-05 15:21:59 -0500

    Add files via upload

---
+++ Functions/Quadratic Solver/function_header.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_HEADER_H
+#define FUNCTION_HEADER_H //define these functions
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+double sqrt(double N, double tol);
+double positiveX(int a, int b, int c, int tol);
+double negative(int a, int b, int c, int tol);
+
+#endif
